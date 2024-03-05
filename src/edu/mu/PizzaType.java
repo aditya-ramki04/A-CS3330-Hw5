c50b41b387afa66bd4367de7487b7020931f42c4
Matthew Robison <mattrobi04@gmail.com>
2024-03-04 18:50:21 -0600

    created PizzaType enum as well as package and main

---
+++ src/edu/mu/PizzaType.java
@@ -0,0 +1,20 @@
+package edu.mu;
+
+public enum PizzaType {
+	HAWAIIAN,
+	MARGHERITA,
+	SUPREME,
+	VEGETARIAN;
+
+	private double toppingPrice;
+	
+	// Constructor
+	private void PizzaTypes(double toppingPrice) {
+	    this.toppingPrice = toppingPrice;
+	}
+	
+	// Getter method
+	public double getToppingPrice() {
+	    return toppingPrice;
+	}
+}
