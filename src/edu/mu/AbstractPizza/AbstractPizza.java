527bc2f864f005146a85d7a28fe2d66927f4dfca
abhi pasupuleti <abhi25.pasupuleti@gmail.com>
2024-03-04 19:19:43 -0600

    Finished the AbstractPizza class

--- src/edu/mu/AbstractPizza/AbstractPizza.java
+++ src/edu/mu/AbstractPizza/AbstractPizza.java
@@ -1,5 +1,74 @@
 package edu.mu.AbstractPizza;
 
-public abstract class AbstractPizza {
+import java.util.*;
 
+public abstract class AbstractPizza 
+{
+	protected List<Toppings> toppingList;
+	protected double priceWithoutToppings;
+    protected double totalPrice;
+    protected int pizzaOrderID;
+    protected static int orderIDCounter = 1000; 
+    protected ICookingStrategy cookingStrategy;
+    protected double cookingPrice;
+    
+    public AbstractPizza() 
+    {
+        this.toppingList = new ArrayList<>();
+        this.totalPrice = 0.0;
+        this.pizzaOrderID = orderIDCounter++;
+    }
+
+    // Setter and getter methods for toppingList
+    public void setToppingList(List<Toppings> toppingList) {
+        this.toppingList = toppingList;
+    }
+
+    public List<Toppings> getToppingList() {
+        return toppingList;
+    }
+
+    // Other setter and getter methods for other attributes
+    public void setPriceWithoutToppings(double priceWithoutToppings) {
+        this.priceWithoutToppings = priceWithoutToppings;
+    }
+
+    public double getPriceWithoutToppings() {
+        return priceWithoutToppings;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public int getPizzaOrderID() {
+        return pizzaOrderID;
+    }
+
+    public static int getOrderIDCounter() {
+        return orderIDCounter;
+    }
+
+    public ICookingStrategy getCookingStrategy() {
+        return cookingStrategy;
+    }
+
+    public void setCookingStrategy(ICookingStrategy cookingStrategy) {
+        this.cookingStrategy = cookingStrategy;
+    }
+
+    public double getCookingPrice() {
+        return cookingPrice;
+    }
+
+    public void setCookingPrice(double cookingPrice) {
+        this.cookingPrice = cookingPrice;
+    }
+
+    // Abstract method for calculating total price
+    public abstract void calculateTotalPrice();
 }
