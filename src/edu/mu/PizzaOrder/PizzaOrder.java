fe475eafe226f9408b091a437684e0db96865dd1
abhi pasupuleti <abhi25.pasupuleti@gmail.com>
2024-03-05 17:51:22 -0600

    Created the PizzaOrder Class

---
+++ src/edu/mu/PizzaOrder/PizzaOrder.java
@@ -0,0 +1,59 @@
+package edu.mu.PizzaOrder;
+
+import edu.mu.AbstractPizza.*;
+
+
+public class PizzaOrder 
+{
+	public void printListOfToppingsByPizzaOrderID(int orderID)
+	{
+		
+	}
+	
+	public void printPizzaOrderCart(int orderID)
+	{
+		
+	}
+	
+	public AbstractPizza getPizzaByOrderID(int orderID)
+	{
+		return null;
+	}
+	
+	public boolean addPizzaToCart(PizzaType pizzaType)
+	{
+		return false;
+		
+	}
+	
+	public boolean addNewToppingToPizza(int orderID, Toppings topping)
+	{
+		return false;
+		
+	}
+	
+	public boolean removeToppingFromPizza(int orderID, Toppings topping)
+	{
+		return false;
+		
+	}
+	
+	public boolean isThereAnyUncookedPizza()
+	{
+		return false;
+		
+	}
+	
+	public double checkout()
+	{
+		return 0;
+		
+	}
+	
+	public boolean selectCookingStrategyByPizzaOrderID(int orderID, CookingStyleType cookingStrategyType)
+	{
+		return false;
+		
+	}
+
+}
